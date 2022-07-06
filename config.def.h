6250b3eec1682a4e6f9ba7f774f6e3e4e56329ea
kawaki <dev@kanjala.com>
2022-07-06 10:58:10 +0200

    gaps and colors

--- config.def.h
+++ config.def.h
@@ -9,10 +9,10 @@ static const unsigned int systrayonleft = 0;	/* 0: systray in the right corner, 
 static const unsigned int systrayspacing = 2;	/* systray spacing */
 static const int systraypinningfailfirst = 1;	/* 1: if pinning fails, display systray on the first monitor,False: display systray on the last monitor */
 static const int showsystray        = 1;	/* 0 means no systray */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -22,14 +22,14 @@ static const unsigned int colorfultitle  = 1;  /* 0 means title use SchemeTitle 
 static const unsigned int colorfultag    = 1;  /* 0 means use SchemeSel for selected tag */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static char normbgcolor[]           = "#222222";
+static const char *fonts[]          = { "monospace:size=11.5", "Font Awesome 6 Free Regular:size:12" };
+static const char dmenufont[]       = "monospace:size=11.5";
+static char normbgcolor[]           = "#282828";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#7daea3";
+static char selbgcolor[]            = "#7daea3";
 static char col_bg[]          = "#1d2021";
 static char col_dark[]        = "#282828";
 static char col_fg[]          = "#d4be98";
@@ -145,7 +145,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", normbgcolor, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 #include "movestack.c"
